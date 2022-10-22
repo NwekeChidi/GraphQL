a3c509c22f7db7b4be5a4c7a930c4ddffd8c46de
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-10-22 18:51:30 +0100

    edit dev script

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
 
 const app = express();
 
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloGraphQLWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello GraphQL World'
+            }
+        })
+    })
+})
+
 app.use('/graphql', graphqlHTTP({
+    schema,
     graphiql: true
 }));
 
